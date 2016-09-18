1153239410531a55cd0b7fc2c2e44bdc4271a9b7
nicolas <nrolland@mac.com>
2016-09-18 16:53:45 +0200

    Commit Test 2

---
+++ src/java/WebServices/ApplicationConfig.java
@@ -0,0 +1,33 @@
+/*
+ * 
+ */
+
+package WebServices;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author nicolas
+ */
+@javax.ws.rs.ApplicationPath("ws")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(WebServices.ReleveResource.class);
+    }
+
+}
