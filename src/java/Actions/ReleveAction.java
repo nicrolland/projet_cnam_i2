fca8bfab1a8f8a94f3d3a39442cf146dfd7b9f1e
nicolas <nrolland@mac.com>
2016-09-23 14:19:52 +0200

    ReleveAction

--- src/java/Actions/ReleveAction.java
+++ src/java/Actions/ReleveAction.java
@@ -5,7 +5,6 @@ package Actions;
 
 import DAO.ReleveDAO;
 import Entities.Releve;
-import Entities.SessionsEnCours;
 import java.util.List;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -14,24 +13,6 @@ import org.json.JSONException;
  * @author nicolas
  */
 public class ReleveAction {
-
-    public int traitReleve(String releveBrut) throws JSONException {
-        Releve releve = new Releve(releveBrut);
-        // Lecture num boitier
-        int numBoitier = releve.getBoitier_id();
-
-        // Recherche session_id
-        int session_id = (new SessionsEnCours()).getSessionIdByBoitier(numBoitier);
-        if (session_id == 0) {
-            return 0;
-        }
-        releve.setSession_id(session_id);
-        // Calcul vitesse
-        // a faire
-        (new ReleveDAO()).insert(releve);
-
-        return 1;
-    }
     
     public JSONArray getAllReleves(String releveBrut) throws JSONException {
         Releve releve = new Releve(releveBrut);
