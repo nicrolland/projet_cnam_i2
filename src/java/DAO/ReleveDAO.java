80b362786fe3cc6c09f8c30a318b17c53f439c6f
Nico <Nico@Nico-PC>
2016-09-23 14:56:37 +0200

    retourne la liste de releves au format json

--- src/java/DAO/ReleveDAO.java
+++ src/java/DAO/ReleveDAO.java
@@ -19,27 +19,25 @@ public class ReleveDAO {
 
     public int insert(Releve releve) {
         int i = 0;
-        try (MongoClient mongo = new MongoClient("localhost", 27017)) {
-            MongoDatabase mongodb = mongo.getDatabase("stpa");
+        MongoDatabase mongodb = MongoBDManager.getMongoDataBase();
+        MongoCollection<Document> colReleve = mongodb.getCollection("releves");
+        Document doc = new Document("session_id", releve.getSession_id())
+                .append("boitier_id", releve.getBoitier_id())
+                .append("datetime", releve.getDatetime())
+                .append("lat", releve.getLat())
+                .append("lon", releve.getLon())
+                .append("mesure0", releve.getMesure0())
+                .append("mesure1", releve.getMesure1())
+                .append("mesure2", releve.getMesure2())
+                .append("mesure3", releve.getMesure3())
+                .append("mesure4", releve.getMesure4())
+                .append("mesure5", releve.getMesure5())
+                .append("mesure6", releve.getMesure6())
+                .append("mesure7", releve.getMesure7())
+                .append("mesure8", releve.getMesure8())
+                .append("mesure9", releve.getMesure9());
+        colReleve.insertOne(doc);
 
-            MongoCollection<Document> colReleve = mongodb.getCollection("releves");
-            Document doc = new Document("session_id", releve.getSession_id())
-                    .append("boitier_id", releve.getBoitier_id())
-                    .append("datetime", releve.getDatetime())
-                    .append("lat", releve.getLat())
-                    .append("lon", releve.getLon())
-                    .append("mesure0", releve.getMesure0())
-                    .append("mesure1", releve.getMesure1())
-                    .append("mesure2", releve.getMesure2())
-                    .append("mesure3", releve.getMesure3())
-                    .append("mesure4", releve.getMesure4())
-                    .append("mesure5", releve.getMesure5())
-                    .append("mesure6", releve.getMesure6())
-                    .append("mesure7", releve.getMesure7())
-                    .append("mesure8", releve.getMesure8())
-                    .append("mesure9", releve.getMesure9());
-            colReleve.insertOne(doc);
-        }
         return i;
     }
 
